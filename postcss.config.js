a6c28a924ec7742e277074bea61780174a2024a4
Faisal Karim <faisalkarim96@gmail.com>
2020-03-15 10:18:26 +0700

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: ["./src/**/*.js"],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      })
+  ]
+};
