b55fef01aac04056f053c90eb91ce40bd48a65d0
Faisal Karim <faisalkarim96@gmail.com>
2020-03-18 22:19:30 +0700

    Add Menu component

---
+++ src/components/Menu/Control.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FaPlus, FaMinus } from 'react-icons/fa'
+
+export default function Control(props) {
+    return (
+      <div className='flex justify-center pb-3'>
+        <button
+          onClick={props.delete}
+          className={
+            props.disabled
+              ? 'flex justify-center bg-blue-300 text-blue-600 w-16 cursor-not-allowed'
+              : 'flex justify-center bg-blue-300 text-blue-900 w-16'
+          }
+          disabled={props.disabled}
+          aria-label='Decrease button'
+        >
+          <FaMinus />
+        </button>
+        <p className='bg-blue-400 w-32 text-blue-900 select-none text-center'>
+          {props.label}
+        </p>
+        <button
+          onClick={props.add}
+          className='flex justify-center text-blue-900 w-16 bg-blue-300'
+          aria-label='Increase button'
+        >
+          <FaPlus />
+        </button>
+      </div>
+    )
+}
---
+++ src/components/Menu/index.js
@@ -0,0 +1,41 @@
+import React, {useContext} from 'react'
+import Control from './Control'
+import { GlobalContext } from '../../context/GlobalState'
+
+const types = [
+    {name: 'Salad', label: 'salad'},
+    {name: 'Bacon', label: 'bacon'},
+    {name: 'Cheese', label: 'cheese'},
+    {name: 'Meat', label: 'meat'}
+]
+
+export const Menu = () => {
+    const { ingredients, price, addIngredient, deleteIngredient } = useContext(GlobalContext)
+    const disabledButton = { ...ingredients }
+
+    for (let key in disabledButton) {
+      disabledButton[key] = disabledButton[key] <= 0
+    }
+
+    return (
+      <div className="bg-blue-200 w-full max-w-sm flex flex-col justify-center p-5 border-content rounded-lg mt-2 lg:mt-0">
+        <p className="text-center text-blue-900 pb-6 font-bold select-none">
+          Price: <span className="text-blue-700">Rp. {price}</span>
+        </p>
+        {types.map(types => (
+          <Control
+            key={types.name}
+            label={types.name}
+            add={() => addIngredient(types.label)}
+            delete={() => deleteIngredient(types.label)}
+            disabled={disabledButton[types.label]}
+          />
+        ))}
+        <div className="flex justify-center pt-2">
+          <button className="bg-blue-600 rounded-lg px-3 py-2 text-blue-100 text-sm">
+            ORDER NOW
+          </button>
+        </div>
+      </div>
+    )
+}
