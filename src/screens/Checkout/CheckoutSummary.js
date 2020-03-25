1b88aeb06a14ecbb874c7ac629c1d4efdf2ababc
Faisal Karim <faisalkarim96@gmail.com>
2020-03-25 22:32:05 +0700

    Add checkout page

---
+++ src/screens/Checkout/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Burger } from '../../components/Burger'
+import PropTypes from 'prop-types'
+
+export default function CheckoutSummary ({ ingredients, cancel, checkout }) {
+  return (
+    <div className='flex flex-col items-center w-full mt-8'>
+      <h1 className='text-gray-700 text-xl'>We hope it tastes well!</h1>
+      <div className='flex justify-center w-full'>
+        <Burger ingredients={ingredients} />
+      </div>
+      <div className='flex'>
+        <button className='bg-red-500 rounded-lg px-3 py-2 text-blue-100 mr-3' onClick={cancel}>CANCEL</button>
+        <button className='bg-blue-500 rounded-lg px-3 py-2 text-blue-100' onClick={checkout}>CONTINUE</button>
+      </div>
+    </div>
+  )
+}
+
+CheckoutSummary.propTypes = {
+  ingredients: PropTypes.object.isRequired,
+  cancel: PropTypes.func.isRequired,
+  checkout: PropTypes.func.isRequired
+}
--- src/screens/Checkout/index.js
+++ src/screens/Checkout/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import { Redirect } from '@reach/router'
+import PropTypes from 'prop-types'
+import CheckoutSummary from './CheckoutSummary'
+import { useBurgerState } from '../../context/BurgerContext'
+import { useOrderState } from '../../context/OrderContext'
 
-export default function Checkout () {
+export default function Checkout ({ navigate }) {
+  const { ingredients } = useBurgerState()
+  const { purchased } = useOrderState()
+
+  const handleCancel = () => {
+    navigate('/', {
+      replace: true
+    })
+  }
+
+  const handleCheckout = () => {
+    navigate('/checkout/form', {
+      replace: true
+    })
+  }
+
+  let summary = <Redirect to='/' noThrow />
+
+  if (ingredients) {
+    const redirectPurchase = purchased && <Redirect to='/' />
+    summary = (
+      <>
+        {redirectPurchase}
+        <CheckoutSummary
+          ingredients={ingredients}
+          cancel={handleCancel}
+          checkout={handleCheckout}
+        />
+      </>
+    )
+  }
+
   return (
     <div className='bg-blue-100 flex flex-col items-center justify-center px-5 pt-5 pb-64 border-content'>
       <h1 className='text-center text-blue-900 text-lg'>
         Checkout Page
       </h1>
+      {summary}
     </div>
   )
+}
+
+Checkout.propTypes = {
+  navigate: PropTypes.func
 }
